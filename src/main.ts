dcb3ce0bc531716cbcdbd27a0e93b6f1970090df
John C <pieboy4444@yahoo.co.uk>
2026-02-03 20:52:35 +0000

    Added a nice rotation "feel" with "weight" behind it.

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ interface Keys {
 
 class GameScene extends Phaser.Scene {
   p!: Phaser.Physics.Matter.Image;
+  ta!: number;
 
   constructor() {
     super("game");
@@ -22,10 +23,18 @@ class GameScene extends Phaser.Scene {
     //this.add.text(100, 100, "Hello Phaser John!");
 
     this.p = this.matter.add.image(100, 100, "player");
-    this.p.setCircle(this.p.width / 2);
+    this.p.setCircle(this.p.width / 2, {
+      restitution: 0.5,
+      friction: 0,
+      frictionAir: 0.01,
+      frictionStatic: 0,
+    });
+    this.p.setMass(30);
 
     this.matter.world.setGravity(0, 0);
     this.matter.world.setBounds(0, 0, 800, 600);
+
+    this.ta = 0;
   }
 
   update() {
@@ -37,23 +46,39 @@ class GameScene extends Phaser.Scene {
     }
 
     let ko = keyboardInput.addKeys("W,S,A,D") as Keys;
-    let force = 0.02;
-
-    if (ko.D.isDown) {
-      console.log("boop2");
-      this.p.applyForce(new Phaser.Math.Vector2(force, 0));
-    }
+    let force = 0.05;
 
     if (ko.W.isDown) {
-      console.log("boop2");
-      //this.p.applyForce(new Phaser.Math.Vector2(0, -force));
-
       this.p.thrustLeft(force);
+    }
+    if (ko.S.isDown) {
+      this.p.thrustRight(force);
+    }
+
+    /// Give the illusion of weight by slowing the rotation of the ship
+    /// when it gets close to 0 anglular velocity by using, auxThrusterPower
+    let auxThrusterPower = 0.05;
+    let targetAngularVelocity = 0;
+
+    let rotateSpeed = 0.1;
+
+    if (ko.D.isDown) {
+      targetAngularVelocity = rotateSpeed;
     }
 
     if (ko.A.isDown) {
-      console.log("boop2");
-      this.p.applyForce(new Phaser.Math.Vector2(-force, 0));
+      targetAngularVelocity = -rotateSpeed;
+    }
+
+    // If we haven't been trying to rotate the ship, activate the
+    // "auxThruster" to stabilise the rotation enough to give the ship
+    // some weight behind it without it feeling sluggish
+    if (targetAngularVelocity != 0) {
+      this.p.setAngularVelocity(targetAngularVelocity);
+    } else {
+      this.p.setAngularVelocity(
+        this.p.getAngularVelocity() * (1 - auxThrusterPower),
+      );
     }
   }
 }
