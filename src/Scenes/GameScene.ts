d7bc44723a341c37a4eb5d7716c199c6632fad9a
John C <pieboy4444@yahoo.co.uk>
2026-02-05 19:31:12 +0000

    Fix #8.
    Will work on putting the arena generating code in it's own class.

--- src/Scenes/GameScene.ts
+++ src/Scenes/GameScene.ts
@@ -1,8 +1,10 @@
 import Asteroid from "../GameObjects/Asteroid";
 import GameBackground from "../GameObjects/GameBackground";
+import Wall from "../GameObjects/Wall";
 
 class GameScene extends Phaser.Scene {
   p!: Phaser.Physics.Matter.Image;
+  statics!: Array<Wall>;
 
   constructor() {
     super("game");
@@ -39,10 +41,26 @@ class GameScene extends Phaser.Scene {
       new Asteroid(this, x * 600, y * 600);
     }
 
+    this.statics = [];
+    // Create Walls
+    let boundSize = 2000;
+    let wallThickness = 100;
+    // Create the "walls", for out of bounds
+    this.statics.push(
+      new Wall(this, 1000, 1000 - boundSize, boundSize * 2, wallThickness),
+    ); ///Top
+    this.statics.push(
+      new Wall(this, 1000 + boundSize, 1000, wallThickness, boundSize * 2),
+    ); // Right
+    this.statics.push(
+      new Wall(this, 1000, 1000 + boundSize, boundSize * 2, wallThickness),
+    ); // Bottom
+    this.statics.push(
+      new Wall(this, 1000 - boundSize, 1000, wallThickness, boundSize * 2),
+    ); // Left
+
     // Turn off gravity (we are in space)
     this.matter.world.setGravity(0, 0);
-
-    this.matter.world.setBounds(-100, -100, 2000, 2000);
   }
 
   update() {
