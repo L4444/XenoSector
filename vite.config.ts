316053f89be9c551b7c98ad0e8a0616b2cf06e83
John C <pieboy4444@yahoo.co.uk>
2026-02-13 17:17:35 +0000

    Base .. attempt 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/XenoSector/",
   server: {
     watch: {
       usePolling: true,
       interval: 100,
     },
   },
-  base: "/XenoSector/",
 });
